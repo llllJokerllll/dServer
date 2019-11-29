0b922d0d59ebdc7baa8f659fb8f260f9cec21bfd
llllJokerllll <Yosi15@gmail.com>
2019-11-29 18:13:10 +0100

    Update

---
+++ bDatos/Trimestre1/Ejercicios1_JMS.sql
@@ -0,0 +1,114 @@
+CREATE DATABASE bd_Xogos;
+USE bd_Xogos;
+DROP DATABASE bd_Xogos;
+
+CREATE TABLE tbl_Estudante (
+	idEstudante varchar NOT NULL CONSTRAINT pk_tbl_Estudante PRIMARY KEY,
+	nome varchar NOT NULL,
+	apelido1 varchar NOT NULL,
+	apelido2 varchar NOT NULL,
+	dataNacemento date NOT NULL,
+	nacionalidade nvarchar(25) NOT NULL,
+	telefono nvarchar(25) NOT NULL,
+	sexo char(2) NOT NULL,
+	tipo nvarchar(25) NOT NULL,
+);
+
+DROP TABLE tbl_Estudante;
+
+CREATE DATABASE bd_TarefasU4;
+USE bd_TarefasU4;
+
+CREATE TABLE tbl_Empregado (
+	dni nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Empregado PRIMARY KEY,
+	nss nvarchar(10) NOT NULL CONSTRAINT uq_tbl_Empregado UNIQUE,
+	nome nvarchar(25) NOT NULL,
+	apelido1 varchar(50) NOT NULL,
+	apelido2 varchar(50) NULL,
+	dataNacemento date NOT NULL,
+	nacionalidade nvarchar(25) NOT NULL,
+	telefono nvarchar(25) NULL,
+	sexo char(2) NOT NULL,
+	tipo nvarchar(25) NULL,
+);
+
+CREATE TABLE tbl_Nacionalidade (
+	codNacionalidade varchar NOT NULL CONSTRAINT pk_tbl_Nacionalidade PRIMARY KEY,
+	codNacion varchar NOT NULL,
+	descricion varchar NULL,
+	idEmpregado nvarchar(10) NOT NULL,
+	CONSTRAINT fk_Nacionalidade_Empregados FOREIGN KEY(idEmpregado) REFERENCES tbl_Empregado(dni) ON DELETE NO ACTION ON UPDATE CASCADE,
+);
+DROP TABLE tbl_Nacionalidade;
+
+CREATE TABLE tbl_Profesor (
+	idProfesor nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Profesor PRIMARY KEY,
+	nome nvarchar(25) NOT NULL,
+	apelido1 varchar(50) NOT NULL,
+	apelido2 varchar(50) NULL,
+	dataNacemento date NOT NULL,
+	enderezoElectronico varchar(50) NULL,
+	corpo varchar NOT NULL,
+	especialidade varchar NOT NULL,
+	telefono nvarchar(25) NULL,
+);
+
+CREATE TABLE tbl_Modulo (
+	codModulo nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Modulo PRIMARY KEY,
+	nome nvarchar(25) NOT NULL,
+	descricion varchar NULL,
+	idProfesor nvarchar(10) NOT NULL,
+	CONSTRAINT fk_Modulo_Profesor FOREIGN KEY(idProfesor) REFERENCES tbl_Profesor(idProfesor) ON DELETE NO ACTION ON UPDATE CASCADE,
+);
+
+CREATE TABLE tbl_Ciclo (
+	codCiclo nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Ciclo PRIMARY KEY,
+	nome nvarchar(25) NOT NULL,
+	descricion varchar NULL,
+);
+
+CREATE TABLE tbl_Ciclo_Modulo (
+	codCicloModulo nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Ciclo_Modulo PRIMARY KEY,
+	codCiclo nvarchar(10) NOT NULL,
+	codModulo nvarchar(10) NOT NULL,
+	observaciones varchar NULL,
+	CONSTRAINT fk_Ciclo_Modulo_Ciclo FOREIGN KEY(codCiclo) REFERENCES tbl_Ciclo(codCiclo) ON DELETE NO ACTION ON UPDATE CASCADE,
+	CONSTRAINT fk_Ciclo_Modulo_Modulo FOREIGN KEY(codModulo) REFERENCES tbl_Modulo(codModulo) ON DELETE NO ACTION ON UPDATE CASCADE,
+);
+
+CREATE TABLE tbl_Piso (
+	codPiso nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Piso PRIMARY KEY,
+	domicilio varchar NOT NULL,
+	localidade varchar NOT NULL,
+	codPostal char(5) NOT NULL,
+);
+
+CREATE TABLE tbl_Contador (
+	codContador nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Contador PRIMARY KEY,
+	modelo varchar(3) NOT NULL,
+	codPiso nvarchar(10) NOT NULL,
+	CONSTRAINT fk_Contador_Piso FOREIGN KEY(codPiso) REFERENCES tbl_Piso(codPiso) ON DELETE NO ACTION ON UPDATE CASCADE,
+);
+
+CREATE TABLE tbl_Estudante (
+	idEstudante nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Estudante PRIMARY KEY,
+	nome nvarchar(25) NOT NULL,
+	apelido1 varchar(50) NOT NULL,
+	apelido2 varchar(50) NULL,
+	dataNacemento date NOT NULL,
+	enderezoElectronico varchar(50) NULL,
+	telefono nvarchar(25) NULL,
+);
+
+CREATE TABLE tbl_Opina (
+	codOpina nvarchar(10) NOT NULL CONSTRAINT pk_tbl_Opina PRIMARY KEY,
+	idProfesor nvarchar(10) NOT NULL,
+	idEstudante nvarchar(10) NOT NULL,
+	codModulo nvarchar(10) NOT NULL,
+	Opinion varchar NULL,
+	CONSTRAINT fk_Opina_Profesor FOREIGN KEY(idProfesor) REFERENCES tbl_Profesor(idProfesor) ON DELETE NO ACTION ON UPDATE CASCADE,
+	CONSTRAINT fk_Opina_Estudante FOREIGN KEY(idEstudante) REFERENCES tbl_Estudante(idEstudante) ON DELETE NO ACTION ON UPDATE CASCADE,
+	CONSTRAINT fk_Opina_Modulo FOREIGN KEY(codModulo) REFERENCES tbl_Modulo(codModulo) ON DELETE NO ACTION,
+);
+
+DROP TABLE tbl_Opina;
---
+++ bDatos/Trimestre1/Prueba1.sql
@@ -0,0 +1,86 @@
+-- SQL DDL - Data Definition Language: CREATE, ALTER, DELETE
+/*
+Nombre: Jose Manuel Sabarís García
+Fecha: 20/11/2019
+Descripción: Ejemplos
+*/
+
+CREATE DATABASE db_prueba;
+USE db_prueba;
+
+/*
+CREATE TABLE tbl_empleados (
+	idEmpleado varchar(7) CONSTRAINT pk_tbl_empleados PRIMARY KEY,
+	nombre varchar(50) NOT NULL,
+	apellido1 varchar(50) NOT NULL,
+	apellido2 varchar(50) NOT NULL,
+	fechaNacimiento date NOT NULL,
+	codOficina int NOT NULL
+);
+*/
+ 
+CREATE TABLE tbl_empleados (
+	idEmpleado varchar(7),
+	nombre varchar(50) NOT NULL,
+	apellido1 varchar(50) NOT NULL,
+	apellido2 varchar(50) NULL,
+	fechaNacimiento date NOT NULL,
+	codOficina int NULL,
+	CONSTRAINT pk_tbl_empleados PRIMARY KEY(idEmpleado),
+	CONSTRAINT fk_empleados_oficina FOREIGN KEY(codOficina) REFERENCES tbl_oficina(codOficina) ON DELETE NO ACTION ON UPDATE CASCADE,
+); 
+--DROP TABLE tbl_empleados;
+CREATE TABLE tbl_oficina (
+	codOficina int NOT NULL,
+	nombreOficina varchar(50) NOT NULL,
+	direccion varchar(70) NOT NULL,
+	codPostal char(5) NOT NULL,
+	telefono char(9) NOT NULL,
+	CONSTRAINT pk_tbl_oficina PRIMARY KEY(codOficina),
+);
+--DROP TABLE tbl_oficina;
+
+--Obtención de las restricciones consultando los esquemas.
+SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS;
+--Obtención restricciones integridad referencial
+SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS;
+
+--USE master;
+--DROP DATABASE db_prueba;
+
+--Obtencion datos BD
+SELECT * FROM tbl_empleados;
+SELECT nombre FROM tbl_empleados;
+
+--Mostrar restricciones
+EXEC sp_helpconstraint tbl_empleados;
+
+--Insertar dentro de una tabla
+INSERT INTO tbl_empleados VALUES('nmop','Luisa','Blanco','Blanco','1967-02-21', '456');
+
+--Utilizar auto-incrementales
+CREATE TABLE tbl_empleados (
+	idEmpleado int IDENTITY (1,1),
+	nombre varchar(50) NOT NULL,
+	apellido1 varchar(50) NOT NULL,
+	apellido2 varchar(50) NULL,
+	fechaNacimiento date NOT NULL,
+	codOficina int NULL,
+	CONSTRAINT pk_tbl_empleados PRIMARY KEY(idEmpleado),
+	CONSTRAINT fk_empleados_oficina FOREIGN KEY(codOficina) REFERENCES tbl_oficina(codOficina) ON DELETE NO ACTION ON UPDATE CASCADE,
+	CONSTRAINT ck_tbl_empleados_fechaNacimiento CHECK (fechaNacimiento < GETDATE()),
+);
+
+CREATE TABLE tbl_oficina (
+	codOficina int IDENTITY (1,1),
+	nombreOficina varchar(50) NOT NULL,
+	direccion varchar(70) NOT NULL,
+	codPostal char(5) NOT NULL,
+	telefono char(9) NOT NULL,
+	ciudad nvarchar(20) NOT NULL,
+	CONSTRAINT pk_tbl_oficina PRIMARY KEY(codOficina),
+	CONSTRAINT uq_tbl_oficina_ciudad UNIQUE(ciudad), --Una oficina SOLO puede estar en una ciudad y las ciudades no se peuden repetir
+);
+
+--Para añadir una restricción tras crear una tabla
+ALTER TABLE tbl_oficina ADD CONSTRAINT df_tbl_oficina_codPostal DEFAULT '32001' FOR codPostal;
